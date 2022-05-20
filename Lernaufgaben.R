474f59e4722d222653f91feb5ca78a044a632b45
samischorch <samighabib@gmail.com>
2022-05-20 11:37:12 +0200

    dMod Test

--- Lernaufgaben.R
+++ Lernaufgaben.R
@@ -6,4 +6,8 @@ library(tidyverse)
 install.packages("dMod")
 library(dMod)
 
+r <- eqnlist()
+
+r <- addReaction(r, from = "A+B", to = "C", rate = "-p1*A*B", description = "Produktion von Komplex C" )
+r
 
